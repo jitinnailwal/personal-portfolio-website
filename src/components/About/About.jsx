d034d0b12247ddef634d88e9c8e34f8985c75626
Jitin Nailwal <101126269+jitinnailwal@users.noreply.github.com>
2025-07-18 00:41:06 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -47,7 +47,7 @@ const About = () => {
             solutions. Let's connect and build something amazing together!
           </p>
 
-          <a href=""
+          <a href="https://drive.google.com/file/d/1ts8W196bzr7xsF9Kxs9chOvdRBtCtfp0/view?usp=drive_link"
             target='_blank'
             rel='noopener noreferrer'
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
